04143416cbaf9d90e38c396a5dad49d3c9ab190e
Maksim Sobolev <sobmig@gmail.com>
2022-07-06 21:09:12 +0300

    закончено написание кода программы с решением через метод с возвращением

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+
+int sum (int num)
+{
+    int summa = 0;
+    int count = num;
+    for (int i = 1; count > 0; i++)
+    {
+        summa = summa + count % 10;
+        count = count / 10;
+    }
+    return summa;
+}
+
+Console.WriteLine ($"{number} {"->"} {sum (number)}");
