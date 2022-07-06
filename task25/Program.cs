2667204d0fd20bd34cc23b45545017ff0ad31ecb
Maksim Sobolev <sobmig@gmail.com>
2022-07-06 19:28:21 +0300

    окончено написание кода программы

---
+++ task25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write ("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+Console.Write ("Введите степень, в которую нужно возвести число: ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+
+double numbersGrade (int num, int i)
+{
+    double grade = Math.Pow (num, i);
+    return grade;
+}
+
+double result = numbersGrade (number, n);
+Console.WriteLine ($"{number} {","} {n} {"->"} {result} {"("} {number} {^n} {")"}");
