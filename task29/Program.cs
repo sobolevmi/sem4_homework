29ff97acc7174f6c7db79dcf4fec685f80091eee
Maksim Sobolev <sobmig@gmail.com>
2022-07-07 00:39:39 +0300

    закончено написание кода программы

---
+++ task29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Объединение 8 введенных элементов в массив и его вывод на экран
+int [] massive ()
+{
+    int [] array = new int [8];
+    for (int index = 0; index < 8; index++)
+    {
+        Console.Write ("Введите элемент массива: ");
+        array [index] = Convert.ToInt32 (Console.ReadLine ());
+    }
+    return array;
+}
+
+void printArray (int [] arr)
+{
+    for (int j = 0; j < arr.Length; j++)
+    {
+        Console.Write (arr [j]+ " ");
+    }
+}
+
+int [] mas = massive ();
+Console.Clear ();
+printArray (mas);
